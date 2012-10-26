974d1b6530a9cc08324b68d6e6fa87d572ec9ca0
ray <ray@geek.local>
2012-10-26 15:22:13 +0800

    new

--- ex2.54.scm
+++ ex2.54.scm
@@ -1,10 +1,15 @@
 (define (equal? a b)
-  (if (or (and (null? a)
-               (not (null? b)))
-          (and (null? b)
-               (not (null? a))))
-      false
-      (if (and (eq? (car a) (car b))
-               (equal? (cdr a) (cdr b)))
-          true
-          false)))
+  (cond ((or (and (null? a)
+                  (not (null? b)))
+             (and (null? b)
+                  (not (null? a))))
+         false)
+        ((and (null? a)
+              (null? b))
+         true)
+        ((and (eq? (car a) (car b))
+              (equal? (cdr a) (cdr b)))
+         true)
+        (else
+         false)))
+
